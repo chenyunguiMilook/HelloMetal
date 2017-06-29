753ac6ad2f44b28400291d402889f5df09e2a233
chenyungui <apple@milook.me>
2017-06-29 18:18:01 +0800

    remove unused code

--- HelloMetal/MySceneViewController.swift
+++ HelloMetal/MySceneViewController.swift
@@ -8,16 +8,44 @@
 
 import UIKit
 
-class MySceneViewController: MetalViewController, MetalViewControllerDelegate {
+class MySceneViewController: UIViewController {
+    
+    var renderer: Renderer!
+    var metalLayer: CAMetalLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        metalViewControllerDelegate = self
+        let device = MTLCreateSystemDefaultDevice()
+        self.renderer = Renderer(device: device!)
+        
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        view.layer.addSublayer(metalLayer)
     }
     
-    // MARK: - MetalViewControllerDelegate
-    func renderObjects(in drawable: CAMetalDrawable) {
-        self.renderer.render(in: drawable)
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        guard let window = view.window else { return }
+        let scale = window.screen.nativeScale
+        let layerSize = view.bounds.size
+        
+        self.view.contentScaleFactor = scale
+        self.metalLayer.frame = CGRect(x: 0, y: 0, width: layerSize.width, height: layerSize.height)
+        self.metalLayer.drawableSize = CGSize(width: layerSize.width * scale, height: layerSize.height * scale)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.render()
+    }
+    
+    func render() {
+        if let drawable = metalLayer.nextDrawable() {
+            self.renderer.render(in: drawable)
+        }
     }
 }
