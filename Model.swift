27bcdd1b3b7330c35006b486d957459e1f855cfb
chenyungui <apple@milook.me>
2017-07-05 14:47:32 +0800

    structure refactoring

--- Model.swift
+++ Model.swift
@@ -19,39 +19,21 @@ public class Model {
     var textureSampler: MTLSamplerState?
     var shader: Shader!
     
-    let availableSources: Int = 3
-    var avaliableResourcesSemaphore: DispatchSemaphore
-    
-    deinit {
-        for _ in 0 ..< availableSources {
-            self.avaliableResourcesSemaphore.signal()
-        }
-    }
-    
-    public init(name: String, library: MTLLibrary, pixelFormat: MTLPixelFormat, geometry: Geometry, texture: MTLTexture?, textureSampler: MTLSamplerState? = nil) {
-        
+    public init(name: String,
+                library: MTLLibrary,
+                pixelFormat: MTLPixelFormat,
+                geometry: Geometry,
+                texture: MTLTexture?,
+                textureSampler: MTLSamplerState? = nil) {
         self.name = name
         self.geometry = GeometryBuffer(geometry: geometry, device: library.device, inflightBuffersCount: availableSources)
         self.shader = Shader(library: library, pixelFormat: pixelFormat)
         self.texture = texture
         self.textureSampler = textureSampler ?? library.device.defaultSampler
-        self.avaliableResourcesSemaphore = DispatchSemaphore(value: availableSources)
     }
     
-    public func render(commandQueue: MTLCommandQueue,
-                       passDescriptor: MTLRenderPassDescriptor,
-                       drawable: CAMetalDrawable) { // means canvas for draw
+    public func render(commandEncoder: MTLRenderCommandEncoder) { // means canvas for draw
         
-        _ = self.avaliableResourcesSemaphore.wait(timeout: DispatchTime.distantFuture)
-        
-        guard let commandBuffer = commandQueue.makeCommandBuffer() else { return }
-        commandBuffer.addCompletedHandler { _ in
-            self.avaliableResourcesSemaphore.signal()
-        }
-        
-        guard let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: passDescriptor) else {
-            return
-        }
         let (vertexBuffer, uvBuffer) = geometry.nextGeometryBuffer()
         commandEncoder.setRenderPipelineState(shader.renderPiplineState)
         commandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
@@ -67,24 +49,43 @@ public class Model {
                                             indexType: .uint32,
                                             indexBuffer: geometry.indexBuffer,
                                             indexBufferOffset: 0)
-        commandEncoder.endEncoding()
-        
-        // the present target could be a MTLTexture
-        commandBuffer.present(drawable)
-        commandBuffer.commit()
     }
 }
 
 public extension Model {
     
-    public convenience init(name: String, library: MTLLibrary, pixelFormat: MTLPixelFormat, geometry: Geometry, texture: String, textureSampler: MTLSamplerState? = nil) {
+    public convenience init(name: String,
+                            library: MTLLibrary,
+                            pixelFormat: MTLPixelFormat,
+                            geometry: Geometry,
+                            texture: String,
+                            textureSampler: MTLSamplerState? = nil) {
+        
         let texture = loadTexture(imageNamed: texture, device: library.device)
-        self.init(name: name, library: library, pixelFormat: pixelFormat, geometry: geometry, texture: texture, textureSampler: textureSampler)
+        
+        self.init(name: name,
+                  library: library,
+                  pixelFormat: pixelFormat,
+                  geometry: geometry,
+                  texture: texture,
+                  textureSampler: textureSampler)
     }
     
-    public convenience init(name: String, library: MTLLibrary, pixelFormat: MTLPixelFormat, geometry: Geometry, texture: UIImage, textureSampler: MTLSamplerState? = nil) {
+    public convenience init(name: String,
+                            library: MTLLibrary,
+                            pixelFormat: MTLPixelFormat,
+                            geometry: Geometry,
+                            texture: UIImage,
+                            textureSampler: MTLSamplerState? = nil) {
+        
         let texture = loadTexture(image: texture, device: library.device)
-        self.init(name: name, library: library, pixelFormat: pixelFormat, geometry: geometry, texture: texture, textureSampler: textureSampler)
+        
+        self.init(name: name,
+                  library: library,
+                  pixelFormat: pixelFormat,
+                  geometry: geometry,
+                  texture: texture,
+                  textureSampler: textureSampler)
     }
 }
 
--- Renderer.swift
+++ Renderer.swift
@@ -10,6 +10,8 @@ import Foundation
 import Metal
 import MetalKit
 
+let availableSources: Int = 3
+
 public class Renderer : NSObject {
 
     var device: MTLDevice!
@@ -17,14 +19,25 @@ public class Renderer : NSObject {
     var commandQueue: MTLCommandQueue!
     var model: ModelPlane!
     
+    var avaliableResourcesSemaphore: DispatchSemaphore!
+    
+    deinit {
+        for _ in 0 ..< availableSources {
+            self.avaliableResourcesSemaphore.signal()
+        }
+    }
+    
     public init(device: MTLDevice) {
         super.init()
         self.device = device
         self.commandQueue = device.makeCommandQueue()
         self.model = ModelPlane(library: device.makeDefaultLibrary()!, pixelFormat: .bgra8Unorm, texture: "cube.png")
+        self.avaliableResourcesSemaphore = DispatchSemaphore(value: availableSources)
     }
     
     public func render(in drawable: CAMetalDrawable) {
+        
+        _ = self.avaliableResourcesSemaphore.wait(timeout: DispatchTime.distantFuture)
         
         let renderPassDescriptor = MTLRenderPassDescriptor()
         renderPassDescriptor.colorAttachments[0].texture = drawable.texture
@@ -32,9 +45,20 @@ public class Renderer : NSObject {
         renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0, 0, 0, 0)
         renderPassDescriptor.colorAttachments[0].storeAction = .store
         
-        self.model.render(commandQueue: commandQueue,
-                          passDescriptor: renderPassDescriptor,
-                          drawable: drawable)
+        guard let commandBuffer = commandQueue.makeCommandBuffer() else { return }
+        commandBuffer.addCompletedHandler { _ in
+            self.avaliableResourcesSemaphore.signal()
+        }
+        
+        guard let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else {
+            return
+        }
+        
+        self.model.render(commandEncoder: commandEncoder)
+        
+        commandEncoder.endEncoding()
+        commandBuffer.present(drawable) // the present target could be a MTLTexture
+        commandBuffer.commit()
     }
 }
 
